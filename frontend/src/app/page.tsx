ce73c337e65df1f1ce7c4e5c9ceed6a1b6649803
Fredrik Öller <oller.fredrik@gmail.com>
2023-09-06 20:48:45 +0200

    init commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,18 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1 className="header"> Scramble Ramble </h1>
+      </div>
+      <div >
+      </div>
+
+      <div>
+      </div>
+
+      <div>
+      </div>
+    </main>
+  )
+}
