526b5406eed51f105c450830023eddc5aa3e5a28
Fredrik Öller <oller.fredrik@gmail.com>
2023-09-16 11:55:29 +0200

    update components

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,21 @@
 "use client";
-import { Button, Hero } from "@/components";
+import { Button, HandleContainer, Hero } from "@/components";
 
 export default function Home() {
+  const data = [
+    { id: 1, handle: "frittzinator" },
+    { id: 2, handle: "machinshin" },
+    { id: 3, handle: "bathamel" },
+    { id: 4, handle: "roboduck" },
+    { id: 5, handle: "gravling" },
+    { id: 6, handle: "trayal" },
+    { id: 7, handle: "boboo" },
+    { id: 8, handle: "darkling" },
+    { id: 9, handle: "nobody" },
+    { id: 10, handle: "mepzon" },
+    { id: 11, handle: "nejon" },
+  ];
+
   const handleLoginClick = () => {
     console.log("clicked login");
   };
@@ -13,18 +27,19 @@ export default function Home() {
   return (
     <main className="">
       <Hero />
-      <div>
-        <Button
-          text="Login"
-          onClick={handleLoginClick}
-          classes="button hover:bg-blue-700 hover:"
-        />
-        <Button
-          text="Register"
-          onClick={handleRegisterClick}
-          classes="button hover:bg-blue-700 hover:"
-        />
-      </div>
+      <Button
+        text="Login"
+        onClick={handleLoginClick}
+        classes="button hover:bg-blue-700 hover:"
+      />
+      <Button
+        text="Register"
+        onClick={handleRegisterClick}
+        classes="button hover:bg-blue-700 hover:"
+      />
+      {data.map((item) => (
+        <HandleContainer key={item.id} userHandle={item.handle} />
+      ))}
     </main>
   );
 }
--- frontend/src/components/Hero/Hero.tsx
+++ frontend/src/components/Hero/Hero.tsx
@@ -1,19 +1,5 @@
+import { Button } from "../Button/Button";
 import "./Hero.css";
-import { HandleContainer } from "../HandleContainer/HandleContainer";
-
-const data = [
-  { id: 1, handle: "frittzinator" },
-  { id: 2, handle: "machinshin" },
-  { id: 3, handle: "bathamel" },
-  { id: 4, handle: "roboduck" },
-  { id: 5, handle: "gravling" },
-  { id: 6, handle: "trayal" },
-  { id: 7, handle: "boboo" },
-  { id: 8, handle: "darkling" },
-  { id: 9, handle: "nobody" },
-  { id: 10, handle: "mepzon" },
-  { id: 11, handle: "nejon" },
-];
 
 export const Hero = () => {
   return (
@@ -22,10 +8,8 @@ export const Hero = () => {
         <div className="hero-text-container">
           <h1 className="hero-text">Scramble-o-rama</h1>
         </div>
+        <Button classes={"button"} text="Hjälp mig!" onClick={() => {console.log("Snälla!")}}></Button>
       </div>
-      {data.map((item) => (
-        <HandleContainer key={item.id} userHandle={item.handle} />
-      ))}
     </div>
   );
 };
